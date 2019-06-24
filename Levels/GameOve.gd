dbfe13f7a2cd787e53cdf666153fb2d8e88bbfba
yoav <yoavhortman5@gmail.com>
2019-06-24 17:58:12 +0200

    Few more tutorials finished

---
+++ Levels/GameOve.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_RestartButton_pressed():
+	get_tree().change_scene("res://Scenes/Levels/Level1.tscn");
--- Player.gd
+++ Player.gd
@@ -6,6 +6,9 @@ const SPEED = 1000;
 const GRAVITY = 300;
 const JUMP_SPEED = 5000;
 const UP = Vector2(0, -1);
+const WORLD_LIMIT = 3000;
+
+var lives = 3;
 
 signal animate;
 
@@ -17,14 +20,19 @@ func _physics_process(delta):
 	move_and_slide(motion, UP);
 	
 func apply_gravity():
-	if not is_on_floor():
-		motion.y += GRAVITY;
-	else:
+	if position.y > WORLD_LIMIT:
+		end_game();
+	if is_on_floor():
 		motion.y = 0;
+	elif is_on_ceiling(): 
+		motion.y = 1;
+	else:
+		motion.y += GRAVITY;
 		
 func jump():
 	if Input.is_action_pressed("Jump") && is_on_floor():
 		motion.y = -JUMP_SPEED;
+		$JumpSFX.play();
 		
 func move():
 	if Input.is_action_pressed("Left") && !Input.is_action_pressed("Right"):
@@ -37,5 +45,17 @@ func move():
 func animate():
 	emit_signal("animate", motion);
 	
+	
+func end_game():
+	get_tree().change_scene("res://Levels/GameOve.tscn");
+	
+func hurt():
+	$PainSFX.play();
+	position.y -= 1;
+	yield(get_tree(), "idle_frame");
+	motion.y = -JUMP_SPEED;
+	lives -= 1;
+	if lives < 0:
+		end_game();
 	
 	
